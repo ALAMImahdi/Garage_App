98b30943dac3f340303b8c763216a3284fb04da9
ALAMImahdi <83419479+ALAMImahdi@users.noreply.github.com>
2022-08-13 12:14:03 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { GarageService } from './garage.service';
+
+import { OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] ,
+  providers : [ GarageService ]
 })
-export class AppComponent {
-  title = 'car';
+export class AppComponent implements OnInit{
+  title = 'Garage_App';
+  cars!: Object;
+
+  constructor(private garageService : GarageService){
+
+  }
+  ngOnInit() {
+    console.log('OnInit....')
+    this.garageService.getCars().subscribe((datas)=>{
+      this.cars = datas;
+    })
+  }
+
+  
 }
